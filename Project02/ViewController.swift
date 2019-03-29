278f4a5903abf57c1d365d18d9627ad2d689d0f8
AlekseyK <aleksey.kabishau@gmail.com>
2019-03-28 22:54:16 -0400

    Initial Commit

---
+++ Project02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by Aleksey on 0328..19.
+//  Copyright © 2019 Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
